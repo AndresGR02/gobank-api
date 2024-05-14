8114f82d3ecd7ddc42a3fe33c9fa897535e515d9
AndresGR02 <andres.gr.workspace@gmail.com>
2024-05-13 19:14:14 -0600

    Create database connection + queries
    Create a connection to a Mongo database and added some functions for creating an account table, adding accounts and quering all accounts

--- api.go
+++ api.go
@@ -3,85 +3,110 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
-    "log"
+
 	"github.com/gorilla/mux"
 )
 
 type APIServer struct {
-    listenAddr string
+	listenAddr string
+	store      Storage
 }
 
+func NewAPIServer(listenAddr string, store Storage) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+		store:      store,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+
+	log.Println("JSON API SERVER RUNNING ON PORT: ", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	}
+	if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	}
+	if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed %s", r.Method)
+}
+
+// GET /account
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	accounts, err := s.store.GetAccounts()
+	if err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, accounts)
+}
+
+func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+
+	fmt.Println(id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	createAccountReq := new(CreateAccountRequest)
+	if err := json.NewDecoder(r.Body).Decode(createAccountReq); err != nil {
+		return err
+	}
+
+	account := NewAccount(createAccountReq.FirstName, createAccountReq.LastName)
+
+	if err := s.store.CreateAccount(account); err != nil {
+		return err
+	}
+
+	return WriteJSON(w, http.StatusOK, account)
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+// This function will transform a response writer into JSON
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
-    w.WriteHeader(status)
-    w.Header().Set("Content-Type", "application/json")
-    return json.NewEncoder(w).Encode(v)
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
 }
 
 // This will be the function signature for all API functions
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
 type ApiError struct {
-    Error string
+	Error string
 }
 
-// This will decorate the apiFunc into an http.HandlerFunc 
+// This will decorate the apiFunc into an http.HandlerFunc
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        if err := f(w, r); err != nil {
-            WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
-        }
-    }
-}
-
-func NewAPIServer(listenAddr string) *APIServer {
-    return &APIServer {
-        listenAddr: listenAddr,
-    }
-}
-
-func (s *APIServer) Run() {
-    router := mux.NewRouter()
-
-    router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-
-    router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
-    
-    log.Println("JSON API SERVER RUNNING ON PORT: ", s.listenAddr)
-
-    http.ListenAndServe(s.listenAddr, router)
-}
-
-func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-    if r.Method == "GET" {
-        return s.handleGetAccount(w, r)
-    }
-    if r.Method == "POST" {
-        return s.handleCreateAccount(w, r)
-    }
-    if r.Method == "DELETE" {
-        return s.handleDeleteAccount(w, r)
-    }
-
-    return fmt.Errorf("method not allowed %s", r.Method)
-}
-
-func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-    id := mux.Vars(r)["id"]
-
-    fmt.Println(id)
-
-    return WriteJSON(w, http.StatusOK, &Account{})
-}
-
-func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-    return nil 
-}
-
-func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
-    return nil
-}
-
-func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
-    return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/AndresGR02/gobank-api
 
 go 1.22.1
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
+)
